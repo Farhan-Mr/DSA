93cc5b064115229d12ae2997a848c57b034dc876
Farhan-Mr <farhat.hp2022@gmail.com>
2025-04-27 12:01:57 +0530

    2nd Method to Delete a element of an array

---
+++ array_ddeelleettee.c
@@ -0,0 +1,55 @@
+/* WAP to delete an element of array */
+
+
+#include<stdio.h>
+
+// Function Decleration for Deletion -
+void delete(int arr[], int n, int del);
+
+// Main function -
+int main()
+{
+    int arr[100];
+    int n, del;
+
+    printf("Enter size of array : ");
+    scanf("%d",&n);
+
+    printf("Enter elements for array : \n");
+        for(int i=0; i<n; i++)
+        {
+            scanf("%d",&arr[i]);
+        }
+    
+        printf("Enter index number of that element which you want to delete it : ");
+        scanf("%d",&del);
+
+        // Function calling -
+        delete(arr,n,del);
+    return 0;
+}
+
+
+void delete(int arr[], int n, int del)
+{
+    printf("Given Array : ");
+    for(int i=0; i<n; i++)
+    {
+        printf("%d ",arr[i]);
+    }
+
+    int j= del;
+    
+    // Delition Process -
+    for(int i=j; i<n; i++)
+    {
+        arr[i] = arr[i+1];
+    }
+
+    printf("\nAfter deletion of given element \nArray is : ");
+    for(int i=0; i<n-1; i++)
+    {
+        printf("%d ",arr[i]);
+    }
+
+}
